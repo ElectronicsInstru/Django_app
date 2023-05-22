e8420454a225693ab0604ecd39fa982983fa1c81
IsaiFomperoza <isaifomperoza@gmail.com>
2023-05-22 15:02:38 -0600

    URLS para blogspot y view hola mundo

--- Web_app/urls.py
+++ Web_app/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 """Aqui se añaden las urls del proyecto en la siguiente lista, sigue el patron"""
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blogpost/', include('blogpost.urls')),
 ]
