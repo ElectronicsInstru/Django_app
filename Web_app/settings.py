9c7c590987ac590c1d41197a4479d85db2013308
IsaiFomperoza <isaifomperoza@gmail.com>
2023-05-24 19:01:02 -0600

    Creacion de html home para blogpost

--- Web_app/settings.py
+++ Web_app/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "blogpost.apps.BlogpostConfig",
+
+
+
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- blogpost/views.py
+++ blogpost/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Hola mundo')
+    return render(request, 'blogpost/home.html')
